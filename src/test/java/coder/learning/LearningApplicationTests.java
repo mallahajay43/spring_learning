fa60c9b544fbcf6f81f51347cba5cecc34073790
root <root@optimus.>
2025-08-02 11:12:39 +0000

    intial

---
+++ src/test/java/coder/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package coder.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
